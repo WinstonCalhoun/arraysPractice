135b6f9c721a390f3efcdc039cffcbc840f2fb56
Winston Calhoun <superdevant@gmail.com>
2022-09-02 19:53:20 -0500

    Call array methods

--- arrays.js
+++ arrays.js
@@ -2,3 +2,12 @@ var students = ["Brad", "Ryan", "Josh", "Eric"];
 var testScores = [35, 60, 75, 90];
 var pass = [true, false]
 var retake = [true, false]
+
+students.pop()
+// extracts the item at the end
+
+testScores.shift()
+// extracts and item from the begining
+
+students.push(..."John")
+// adds and item at the end
